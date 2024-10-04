5b4f5d3b9315e5d5e54b42f571ee8b0c8e944023
Neharika Shah <nsha0102@student.monash.edu>
2024-10-04 13:46:43 +1000

    add and document new function

--- R/run-app.R
+++ R/run-app.R
@@ -3,3 +3,18 @@ run_app <- function() {
   app_dir <- system.file("eda-app", package = "cwdata")
   shiny::runApp(app_dir, display.mode = "normal")
 }
+
+#' @title
+#' @description
+#' fgfggfg-a short description
+#' @returns Nothing
+#' @examples
+#' \dontrun{
+#'    run_my_app()
+#' }
+#' @export
+run_my_app <-function() {
+  app_dir <- system.file("my-app", package = "cwdata")
+  shiny::runApp(app_dir, display.mode = "normal")
+}
+
